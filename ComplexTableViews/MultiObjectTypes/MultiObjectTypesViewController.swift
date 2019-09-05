597ff5f3a75bd00be4d6c6286c8fb476acd5d525
Nutdanai Vankrua <nutdanai@odd.works>
2019-09-05 23:36:13 +0700

    add tableview into multi-object types view controller

--- ComplexTableViews/MultiObjectTypes/MultiObjectTypesViewController.swift
+++ ComplexTableViews/MultiObjectTypes/MultiObjectTypesViewController.swift
@@ -2,9 +2,24 @@ import UIKit
 
 class MultiObjectTypesViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        title = "Multi Object Types"
+        
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
     
 }
