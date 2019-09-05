fb29cd89a7b4663a24dc3e47423c1af0d0eb268e
Nutdanai Vankrua <nutdanai@odd.works>
2019-09-05 23:27:11 +0700

    introduce view controller with table view using multiple object types

--- ComplexTableViews/AppDelegate.swift
+++ ComplexTableViews/AppDelegate.swift
@@ -16,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Prepare root view controller
+        let rootViewController = MultiObjectTypesViewController()
+        
+        // Set main screen
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = rootViewController
+        
         return true
     }
 
---
+++ ComplexTableViews/MultiObjectTypes/MultiObjectTypesViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MultiObjectTypesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
